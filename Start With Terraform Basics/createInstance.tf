c4b1edfd0ce4750ce2bde37bb23467197bf1a72a
greenguy <raviimahavig@gmail.com>
2024-01-21 15:30:34 +0800

    full

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,10 +1,10 @@
+provider "aws" {
+  access_key = "AKIA6ODUZYNXMXGYNB3A"
+  secret_key = "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"
+  region = "ap-southeast-1"
+}
 
-resource "aws_instance" "MyFirstInstnace" {
-  count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
-
-  tags = {
-    Name = "demoinstnce-${count.index}"
-  }
+resource "aws_instance" "fullterraform" {
+  ami           = "ami-00f19babdd7a36581"
+  instance_type = "t2.micro"
 }
